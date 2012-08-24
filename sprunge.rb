8bccc8a79de390256418cf242793aae97c2c34b9
Damiano Stoffie <damiano@insiberia.net>
2012-08-25 01:31:31 +0200

    fixed tabs

--- sprunge.rb
+++ sprunge.rb
@@ -21,43 +21,43 @@ entries = 10
 collection = Mongo::Connection.new.db(database)['sprunge']
 
 get '/' do
-  # show your homepage here
-  redirect to('http://www.google.com')
+	# show your homepage here
+	redirect to('http://www.google.com')
 end
 
 get %r{^/([A-Za-z0-9\-_]{16})$} do |base64_id|
-  # decode the id
-  id = BSON::ObjectId.new(Base64.urlsafe_decode64(base64_id).bytes.to_a)
-  document = collection.find(:_id => id).first
+	# decode the id
+	id = BSON::ObjectId.new(Base64.urlsafe_decode64(base64_id).bytes.to_a)
+	document = collection.find(:_id => id).first
 
-  text = document['text']
-  lang = request.GET.keys.first
-  
-  if document and lang
-    body CodeRay::Duo[lang, :div].highlight(text)
-  elsif document
-    headers 'Content-Type' => 'text/plain'
-    body text
-  else
-    redirect to('/')
-  end
+	text = document['text']
+	lang = request.GET.keys.first
+	
+	if document and lang
+		body CodeRay::Duo[lang, :div].highlight(text)
+	elsif document
+		headers 'Content-Type' => 'text/plain'
+		body text
+	else
+		redirect to('/')
+	end
 end
 
 post '/' do
-  if collection.find.count >= entries
-    # find and remove the oldest document
-    document = collection.find.sort([:date, :desc]).first
-    collection.remove(document)
-  end
+	if collection.find.count >= entries
+		# find and remove the oldest document
+		document = collection.find.sort([:date, :desc]).first
+		collection.remove(document)
+	end
 
-  # insert the new document 
-  document = {:date => Time.now, :text => request.POST[post_key]} 
-  id = collection.insert(document)
-  # get the encoded id
-  base64_id = Base64.urlsafe_encode64(id.to_a.map{ |i| i.chr }.join)
-  response_url = 'http://' + domain + '/' + base64_id + "\n"
+	# insert the new document 
+	document = {:date => Time.now, :text => request.POST[post_key]} 
+	id = collection.insert(document)
+	# get the encoded id
+	base64_id = Base64.urlsafe_encode64(id.to_a.map{ |i| i.chr }.join)
+	response_url = 'http://' + domain + '/' + base64_id + "\n"
 
-  headers 'Content-Type' => 'text/plain'
-  body response_url
+	headers 'Content-Type' => 'text/plain'
+	body response_url
 end
 
